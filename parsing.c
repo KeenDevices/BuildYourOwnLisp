068efc266d71dcd98e8fdc375805a5ed73ebc694
Nick Griffin <nick.griffin@protolabs.com>
2019-02-04 17:07:57 -0600

    Added langage code for Polish Notation

--- parsing.c
+++ parsing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "./mpc/mpc.h"
 
 // If compiling for Windows
 #ifdef _WIN32
@@ -27,6 +28,22 @@ void add_history(char *unused) {}
 
 int main(int argc, char **argv)
 {
+    /* Create new some new parsers */
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lispy     = mpc_new("lispy");
+
+    /* Define the following language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                       \
+    number      : /-?[0-9]+/ ;                              \
+    operator    : '+' | '-' | '*' | '/' ;                   \
+    expr        : <number> | '(' <operator> <expr>+ ')' ;   \
+    lispy       : /^/ <operator> <expr>+ /$/ ;              \
+    ",
+    Number, Operator, Expr, Lispy);
+
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
@@ -40,6 +57,8 @@ int main(int argc, char **argv)
 
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
