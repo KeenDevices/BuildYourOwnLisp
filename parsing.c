52cc6fa29900e0dbce614accd50c91d36623f1e4
Nick Griffin <nick.griffin@protolabs.com>
2019-02-04 14:23:35 -0600

    Added initial parsing.c code

---
+++ parsing.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// If compiling for Windows
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char *readline(char *prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* unused */
+void add_history(char *unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif // _WIN32 or Linux/Unix/MacOS
+
+int main(int argc, char **argv)
+{
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    while(1)
+    {
+        char *input = readline("lispy> ");
+
+        add_history(input);
+
+        printf("No you're a %s\n", input);
+
+        free(input);
+    }
+
+    return 0;
+}
