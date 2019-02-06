9d4e708876d2463aed83cb825ff5798ca359d180
KeenDevices <keendevices@hotmail.com>
2019-02-05 21:27:25 -0600

    Aligning repositories

--- Ch5/parsing.c
+++ Ch5/parsing.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./mpc/mpc.h"
+#include "../mpc/mpc.h"
 
-// If compiling for Windows
 #ifdef _WIN32
 #include <string.h>
 
@@ -12,37 +11,36 @@ char *readline(char *prompt)
 {
     fputs(prompt, stdout);
     fgets(buffer, 2048, stdin);
-    char *cpy = malloc(strlen(buffer) + 1);
+    char *cpy = malloc(strlen(buffer)+1);
     strcpy(cpy, buffer);
-    cpy[strlen(cpy) - 1] = '\0';
+    cpy[strlen(coy)-1] = '\0';
     return cpy;
 }
 
-/* unused */
 void add_history(char *unused) {}
 
 #else
 #include <editline/readline.h>
 #include <editline/history.h>
-#endif // _WIN32 or Linux/Unix/MacOS
+#endif
 
 int main(int argc, char **argv)
 {
-    /* Create new some new parsers */
-    mpc_parser_t* Number    = mpc_new("number");
-    mpc_parser_t* Operator  = mpc_new("operator");
-    mpc_parser_t* Expr      = mpc_new("expr");
-    mpc_parser_t* Lispy     = mpc_new("lispy");
+    /* Create some parsers */
+    mpc_parser_t *Number    = mpc_new("number");
+    mpc_parser_t *Operator  = mpc_new("operator");
+    mpc_parser_t *Expr      = mpc_new("expr");
+    mpc_parser_t *Lispy     = mpc_new("lispty");
 
-    /* Define the following language */
+    /* Defin the parsers with the following language */
     mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                       \
-    number      : /-?[0-9]+/ ;                              \
-    operator    : '+' | '-' | '*' | '/' ;                   \
-    expr        : <number> | '(' <operator> <expr>+ ')' ;   \
-    lispy       : /^/ <operator> <expr>+ /$/ ;              \
-    ",
-    Number, Operator, Expr, Lispy);
+        "                                               \
+        number : /-?[0-9}+/;                            \
+        opertor: '+' | '-' | '*', '/';                  \
+        expr: <number> | '(' <operator> <expr>+ ')';    \
+        lispy: /^/ <operator> <expr>+ /$/;              \
+        ",
+        Number, Operator, Expr, Lispy);
 
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
@@ -50,20 +48,19 @@ int main(int argc, char **argv)
     while(1)
     {
         char *input = readline("lispy> ");
-
         add_history(input);
 
-        /* Attempt to parse the user input */
+        /* attempt to parse user input*/
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r))
         {
-            /* On success print the AST */
+            /* on success print the AST */
             mpc_ast_print(r.output);
             mpc_ast_delete(r.output);
         }
         else
         {
-            /* Otherwise print the error */
+            /* otherwise print the error */
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
